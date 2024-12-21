7e2cdc10baeec9389a9fce443288f972d58e1988
ant <anthonydalvarado@gmail.com>
2024-12-21 17:20:05 -0500

    created post models

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -1,3 +1,31 @@
 from django.db import models
-
+from users.models import Member
 # Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(Member, on_delete=models.CASCADE)
+    content = models.TextField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Post created by {self.user.username} on {self.created_at}"
+    
+
+class Comment(models.Model):
+    user = models.ForeignKey(Member, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on {self.post.user.username}'s post"
+    
+
+class Like(models.Model):
+    user = models.ForeignKey(Member, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Like by {self.user.username} on {self.post.user.username}'s post"
+    
